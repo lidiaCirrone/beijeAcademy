9309927f8ddf5a27efe49b24f704eef5fa61b8ae
Laptop <lidia.cirrone@gmail.com>
2022-04-08 17:59:53 +0200

    core: search bar, state object and results update while typing

--- web/search/src/screens/home/Home.js
+++ web/search/src/screens/home/Home.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // COMPONENTS
+import UiInputBox from '../../components/hookComponents/ui/UiInputBox';
 
 // STYLES
 import './Home.css';
 
+// UTILS
+import { getTVSeries } from '../../utils/utils';
+
 
 function Home() {
 
+   const data = getTVSeries();
+
+   const [state, setState] = useState({
+      searchString: '',
+      visibleData: data
+   });
+
+   const inputCallback = (string) => {
+      console.log(string);
+      setState({
+         ...state,
+         searchString: string.toLowerCase,
+         visibleData: data.filter(item => item.name.toLowerCase().startsWith(string.toLowerCase()))
+         // TO-DO use .match() to avoid .toLowerCase()
+         // visibleData: data.filter(item => item.name.match(`/${string}/i`))
+      })
+   }
+
+   const renderListItem = (item, key) => {
+      return (
+         <li key={key}>
+            <span>{item.name}</span>
+            <span>{item.seasons} seasons</span>
+            <span>first aired in {item.year}</span>
+         </li>
+      )
+   }
+
    return (
       <main>
-         Test
+         <UiInputBox
+            placeholder={'Search...'}
+            callback={inputCallback}
+         />
+         <ul>
+            {state.visibleData.map(renderListItem)}
+         </ul>
       </main>
    );
 }
