fbb66036531c5cc3b9e85f6a53df835ce34a99c4
Laptop <lidia.cirrone@gmail.com>
2022-05-02 12:50:44 +0200

    api-testing: axios post request, both promise and async/await functions

--- web/api-testing/src/index.js
+++ web/api-testing/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import Home from './screens/home/Home';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <React.StrictMode>
-      <Home />
-   </React.StrictMode>
+   <Home />
 );
--- web/api-testing/src/screens/home/Home.js
+++ web/api-testing/src/screens/home/Home.js
@@ -16,7 +16,6 @@ function Home() {
    useEffect(() => {
       axios.get('https://jsonplaceholder.typicode.com/posts')
          .then(function (response) {
-            // handle success
             setState({
                ...state,
                posts: response.data
@@ -29,16 +28,69 @@ function Home() {
          })
    }, [])
 
+   const renderArticles = (post, key) => {
+      return (
+         <div key={`post-${key}`}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+         </div>
+      )
+   }
+
+   const addPost = () => {
+      axios.post('https://jsonplaceholder.typicode.com/posts',
+         JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+         }),
+         {
+            headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+            }
+         }
+      )
+         .then(function (response) {
+            console.log(response.data);
+            let updatedPosts = state.posts;
+            updatedPosts.push(response.data);
+            setState({
+               ...state,
+               posts: updatedPosts
+            })
+         })
+         .catch(function (error) {
+            console.log(error);
+         });
+   }
+
+   const asyncAddPost = async () => {
+      let posts = await axios.post('https://jsonplaceholder.typicode.com/posts',
+         JSON.stringify({
+            title: 'foo2',
+            body: 'bar2',
+            userId: 2,
+         }),
+         {
+            headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+            }
+         }
+      )
+      console.log(posts.data);
+      let updatedPosts = state.posts;
+      updatedPosts.push(posts.data);
+      setState({
+         ...state,
+         posts: updatedPosts
+      })
+   }
+
    return (
       <main>
-         {state.posts.map((post, key) => {
-            return (
-               <div key={`post-${key}`}>
-                  <h3>{post.title}</h3>
-                  <p>{post.body}</p>
-               </div>
-            )
-         })}
+         <button onClick={addPost}>Add post (then & catch)</button>
+         <button onClick={asyncAddPost}>Add post (async await)</button>
+         {state.posts.map(renderArticles)}
       </main>
    );
 }
