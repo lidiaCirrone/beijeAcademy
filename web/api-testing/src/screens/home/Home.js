31aaf5dbb6f5d3e785bdace79bffae2d7d84418d
Laptop <lidia.cirrone@gmail.com>
2022-05-02 10:34:19 +0200

    api-testing: axios get request

--- web/api-testing/src/screens/home/Home.js
+++ web/api-testing/src/screens/home/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // COMPONENTS
 
@@ -8,9 +9,36 @@ import './Home.css';
 
 function Home() {
 
+   const [state, setState] = useState({
+      posts: []
+   });
+
+   useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+         .then(function (response) {
+            // handle success
+            setState({
+               ...state,
+               posts: response.data
+            })
+            console.log(response.data);
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         })
+   }, [])
+
    return (
       <main>
-         Test
+         {state.posts.map((post, key) => {
+            return (
+               <div key={`post-${key}`}>
+                  <h3>{post.title}</h3>
+                  <p>{post.body}</p>
+               </div>
+            )
+         })}
       </main>
    );
 }
