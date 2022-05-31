a7f7a0effd6426133c1ac844b3ae34f03d0bc378
Lidia Cirrone <lidia.cirrone@gmail.com>
2022-05-31 15:40:46 +0200

    expo-app: exported styles

--- mobile/expo-app/App.tsx
+++ mobile/expo-app/App.tsx
@@ -1,26 +1,17 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import styleApp
+ from './styleApp';
 
 const App: FunctionComponent = () => {
+
    return (
-      <View style={styles.container}>
-         <Text style={styles.sentence}>Open up App.tsx to start working on your app!</Text>
+      <View style={styleApp.container}>
+         <Text style={styleApp.sentence}>Open up App.tsx to start working on your app!</Text>
          <StatusBar style="auto" />
       </View>
    );
 }
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#39bdff',
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
-   sentence: {
-      color: '#fff'
-   }
-});
 
 export default App;
---
+++ mobile/expo-app/styleApp.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create(
+   {
+      container: {
+         flex: 1,
+         backgroundColor: '#39bdff',
+         alignItems: 'center',
+         justifyContent: 'center',
+      },
+      sentence: {
+         color: '#fff'
+      }
+   }
+)
