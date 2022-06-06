581c4605ed2041ce8978b4687854d0ce1c80385c
Lidia Cirrone <lidia.cirrone@gmail.com>
2022-06-06 11:36:43 +0200

    expo-camera: camera handling

--- mobile/expo-camera/App.tsx
+++ mobile/expo-camera/App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+interface State {
+   hasPermission: boolean;
+   cameraType: CameraType;
+}
+
+const initialState: State = {
+   hasPermission: false,
+   cameraType: CameraType.back
+}
+
+const App: FunctionComponent = () => {
+
+   const [state, setState] = useState<State>(initialState);
+
+   const requestCameraPerm = async (): Promise<void> => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setState({
+         ...state,
+         hasPermission: status === 'granted'
+      })
+   }
+   useEffect(() => {
+      requestCameraPerm()
+   }, [])
+
+   const switchCameraType = () => {
+      setState({
+         ...state,
+         cameraType: state.cameraType === CameraType.back ? CameraType.front : CameraType.back
+      });
+   }
+
+   return (
+      <View style={styles.container}>
+         <Text>Camera Testing</Text>
+         {state.hasPermission &&
+            <Camera type={state.cameraType}>
+               <View>
+                  <TouchableOpacity
+                     onPress={switchCameraType}>
+                     <Text>Flip</Text>
+                  </TouchableOpacity>
+               </View>
+            </Camera>
+         }
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
+
+export default App;
