33f52caf42dd104c22fd2118690937d3b300d138
Lidia Cirrone <lidia.cirrone@gmail.com>
2022-06-07 14:25:34 +0200

    sos-app: conversion to typescript of basic src

--- mobile/sos-app/App.tsx
+++ mobile/sos-app/App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {FunctionComponent} from 'react';
 
-export default function App() {
+// modules
+import { StatusBar } from 'expo-status-bar';
+import { Text, SafeAreaView } from 'react-native';
+
+// styles
+import styleApp from './styleApp';
+
+
+const App: FunctionComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styleApp.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
